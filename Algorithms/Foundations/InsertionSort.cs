ead60d94759c6ca8d7ad2536f71acffb97256cf3
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-04-29 14:16:08 +0300

    Convert InsertionSort.Sort() to generic

--- Algorithms/Foundations/InsertionSort.cs
+++ Algorithms/Foundations/InsertionSort.cs
@@ -1,15 +1,18 @@
-﻿namespace Algorithms.Foundations
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Foundations
 {
     public static class InsertionSort
     {
-        public static void Sort(int[] collection)
+        public static void Sort<T>(IList<T> collection) where T : IComparable
         {
-            for (var i = 1; i < collection.Length; i++)
+            for (var i = 1; i < collection.Count; i++)
             {
                 var key = collection[i];
                 var j = i - 1;
 
-                while (j >= 0 && collection[j] > key)
+                while (j >= 0 && collection[j].CompareTo(key) == 1)
                 {
                     collection[j + 1] = collection[j];
                     j--;
--- TestAlgorithms/Foundations/InsertionSortTests.cs
+++ TestAlgorithms/Foundations/InsertionSortTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Algorithms.Foundations;
 using FluentAssertions;
 using NUnit.Framework;
@@ -12,13 +13,23 @@ namespace TestAlgorithms.Foundations
         }
 
         [Test]
-        public void Sort_SomeCollection_SortedCollection()
+        public void Sort_SomeIntArray_SortedIntArray()
         {
             var array = new[] {2, 4, 5, 6, 1, 3};
 
             InsertionSort.Sort(array);
 
             array.Should().Equal(1, 2, 3, 4, 5, 6);
+        }
+        
+        [Test]
+        public void Sort_SomeStringList_SortedStringList()
+        {
+            var array = new List<string> {"B", "D", "E", "F", "A", "C"};
+
+            InsertionSort.Sort(array);
+
+            array.Should().Equal("A", "B", "C", "D", "E", "F");
         }
     }
 }
