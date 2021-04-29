603b73d0491cfa518a8d9f8bb26d8c318dc708cb
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-04-29 16:35:57 +0300

    Merge Sort

---
+++ Algorithms/Foundations/MergeSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Foundations
+{
+    public static class MergeSort
+    {
+        public static void Sort<T>(IList<T> input) where T : IComparable
+        {
+            Sort(input, 0, input.Count - 1);
+        }
+        
+        private static void Sort<T>(IList<T> input, int left, int right) where T : IComparable
+        {
+            if (left >= right) return;
+            
+            var middle = (left + right) / 2;
+            
+            Sort(input, left, middle);
+            Sort(input, middle + 1, right);
+            Merge(input, left, middle, right);
+        }
+
+        private static void Merge<T>(IList<T> input, int left, int middle, int right) where T : IComparable
+        {
+            var leftCollection = new T[middle - left + 1];
+            var rightCollection = new T[right - middle];
+
+            for (var i = 0; i < leftCollection.Length; i++)
+            {
+                leftCollection[i] = input[left + i];
+            }
+            for (var i = 0; i < rightCollection.Length; i++)
+            {
+                rightCollection[i] = input[middle + i + 1];
+            }
+
+            for (int k = left, i = 0, j = 0; k <= right; k++)
+            {
+                if (i == leftCollection.Length)
+                {
+                    input[k] = rightCollection[j++];
+                }
+                else if (j == rightCollection.Length)
+                {
+                    input[k] = leftCollection[i++];
+                }
+                else if (leftCollection[i].CompareTo(rightCollection[j]) is -1 or 0)
+                {
+                    input[k] = leftCollection[i++];
+                }
+                else
+                {
+                    input[k] = rightCollection[j++];
+                }
+            }
+        }
+    }
+}
---
+++ TestAlgorithms/Foundations/MergeSortTests.cs
@@ -0,0 +1,24 @@
+﻿using Algorithms.Foundations;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestAlgorithms.Foundations
+{
+    public class MergeSortTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Sort_SomeCollection_SortedCollection()
+        {
+            var input = new[] {5, 2, 4, 7, 1, 3, 2, 6};
+            
+            MergeSort.Sort(input);
+
+            input.Should().Equal(1, 2, 2, 3, 4, 5, 6, 7);
+        }
+    }
+}
