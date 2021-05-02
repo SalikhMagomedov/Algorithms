0fc9031876054c7a72fb213ac5751e8de376c04b
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-05-02 11:51:43 +0300

    FindMaximumSubarray.cs

---
+++ Algorithms/Foundations/FindMaximumSubarray.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Foundations
+{
+    public static class FindMaximumSubarray
+    {
+        public static (int low, int high, int sum) Find(int[] input)
+        {
+            return Find(input, 0, input.Length - 1);
+        }
+
+        private static (int low, int hight, int sum) Find(IReadOnlyList<int> input, int low, int high)
+        {
+            if (low == high) return (low, high, input[low]);
+
+            var middle = (low + high) / 2;
+            var left = Find(input, low, middle);
+            var right = Find(input, middle + 1, high);
+            var cross = FindMaxCrossingSubarray(input, low, middle, high);
+
+            if (left.sum >= right.sum && left.sum >= cross.sum) return left;
+            if (right.sum >= left.sum && right.sum >= cross.sum) return right;
+
+            return cross;
+        }
+
+        private static (int low, int hight, int sum) FindMaxCrossingSubarray(IReadOnlyList<int> input, int low, int middle, int high)
+        {
+            var sum = 0;
+            var leftSum = int.MinValue;
+            var maxLeft = low;
+
+            for (var i = middle; i >= low; i--)
+            {
+                sum += input[i];
+                
+                if (sum <= leftSum) continue;
+                
+                leftSum = sum;
+                maxLeft = i;
+            }
+
+            var rightSum = int.MinValue;
+            var maxRight = high;
+
+            sum = 0;
+            for (var i = middle + 1; i <= high; i++)
+            {
+                sum += input[i];
+                
+                if (sum <= rightSum) continue;
+
+                rightSum = sum;
+                maxRight = i;
+            }
+
+            return (maxLeft, maxRight, leftSum + rightSum);
+        }
+    }
+}
---
+++ TestAlgorithms/Foundations/FindMaximumSubarrayTests.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.Foundations;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestAlgorithms.Foundations
+{
+    public class FindMaximumSubarrayTests
+    {
+        [Test]
+        public void Find_SomeContainer_MaximumSubarrayIndexesAndSum()
+        {
+            var input = new[] {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
+
+            var result = FindMaximumSubarray.Find(input);
+
+            result.Should().Be((7, 10, 43));
+        }
+    }
+}
