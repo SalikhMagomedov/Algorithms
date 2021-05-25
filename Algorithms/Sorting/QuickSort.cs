e0cf22779fe6cb65b2a5fb4a1e1f72e75e1ee9b7
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-05-25 22:18:59 +0300

    QuickSort.cs

---
+++ Algorithms/Sorting/QuickSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+    public static class QuickSort
+    {
+        public static void Sort<T>(IList<T> input) where T : IComparable
+        {
+            Sort(input, 0, input.Count - 1);
+        }
+
+        private static void Sort<T>(IList<T> input, int left, int right) where T : IComparable
+        {
+            if (left >= right) return;
+            
+            var partition = Partition(input, left, right);
+
+            Sort(input, left, partition - 1);
+            Sort(input, partition + 1, right);
+        }
+
+        private static int Partition<T>(IList<T> input, int left, int right) where T : IComparable
+        {
+            var x = input[right];
+            var i = left - 1;
+
+            for (var j = left; j < right; j++)
+            {
+                if (input[j].CompareTo(x) == 1) continue;
+                
+                i++;
+                (input[i], input[j]) = (input[j], input[i]);
+            }
+
+            (input[i + 1], input[right]) = (input[right], input[i + 1]);
+
+            return i + 1;
+        }
+    }
+}
---
+++ TestAlgorithms/Sorting/QuickSortTests.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.Sorting;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestAlgorithms.Sorting
+{
+    public class QuickSortTests
+    {
+        [Test]
+        public void Sort_SomeCollection_SortedCollection()
+        {
+            var input = new[] {2, 8, 7, 1, 3, 5, 6, 4};
+
+            QuickSort.Sort(input);
+
+            input.Should().Equal(1, 2, 3, 4, 5, 6, 7, 8);
+        }
+    }
+}
