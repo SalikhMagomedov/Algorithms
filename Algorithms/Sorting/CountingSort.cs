b335b76baa90b6d76b911c77e73ffa3f2578970b
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-05-31 14:56:13 +0300

    CountingSort.cs

---
+++ Algorithms/Sorting/CountingSort.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+    public static class CountingSort
+    {
+        public static IEnumerable<int> Sort(int[] input, int maxInt)
+        {
+            var result = new int[input.Length];
+            var temp = new int[maxInt + 1];
+
+            foreach (var e in input) temp[e]++;
+
+            for (var i = 1; i < temp.Length; i++) temp[i] += temp[i - 1];
+
+            for (var i = input.Length - 1; i >= 0; i--)
+            {
+                result[temp[input[i]] - 1] = input[i];
+                temp[input[i]]--;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestAlgorithms/Sorting/CountingSortTests.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.Sorting;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestAlgorithms.Sorting
+{
+    public class CountingSortTests
+    {
+        [Test]
+        public void Sort_SomeIntArray_SortedIntArray()
+        {
+            var input = new[] {2, 5, 3, 0, 2, 3, 0, 3};
+
+            var result = CountingSort.Sort(input, 5);
+
+            result.Should().Equal(0, 0, 2, 2, 3, 3, 3, 5);
+        }
+    }
+}
