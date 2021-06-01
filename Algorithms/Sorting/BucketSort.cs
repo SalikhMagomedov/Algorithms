0f5e7344ac142953138507509e420ee42339487f
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-06-01 10:26:52 +0300

    BucketSort.cs

---
+++ Algorithms/Sorting/BucketSort.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+    public static class BucketSort
+    {
+        public static IEnumerable<double> Sort(double[] input)
+        {
+            var result = new List<double>();
+            var bucket = new List<List<double>>();
+            var n = input.Length;
+
+            for (var i = 0; i < n; i++)
+            {
+                bucket.Add(new List<double>());
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                bucket[(int) (n * input[i])].Add(input[i]);
+            }
+            
+            bucket.ForEach(InsertionSort.Sort);
+            bucket.ForEach(result.AddRange);
+
+            return result;
+        }
+    }
+}
---
+++ TestAlgorithms/Sorting/BucketSortTests.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.Sorting;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestAlgorithms.Sorting
+{
+    public class BucketSortTests
+    {
+        [Test]
+        public void Sort_SomeDoubleArrayFromZeroToOne_SortedDoubleArray()
+        {
+            var input = new[] {.78, .17, .39, .26, .72, .94, .21, .12, .23, .68};
+
+            var result = BucketSort.Sort(input);
+
+            result.Should().Equal(.12, .17, .21, .23, .26, .39, .68, .72, .78, .94);
+        }
+    }
+}
