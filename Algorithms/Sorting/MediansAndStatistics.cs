5fdb2f34f9d46d9a4855739b45387b818d7b9018
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-06-01 15:59:30 +0300

    Min and Max value from container

---
+++ Algorithms/Sorting/MediansAndStatistics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+    public static class MediansAndStatistics
+    {
+        public static T Minimum<T>(this IList<T> input) where T : IComparable
+        {
+            return input.Extremum((min, i) => min.CompareTo(i) == 1);
+        }
+
+        public static T Maximum<T>(this IList<T> input) where T : IComparable
+        {
+            return input.Extremum((max, i) => max.CompareTo(i) == -1);
+        }
+
+        private static T Extremum<T>(this IList<T> input, Func<T, T, bool> comparison)
+        {
+            var result = input[0];
+
+            for (var i = 1; i < input.Count; i++)
+            {
+                if (!comparison(result, input[i])) continue;
+
+                result = input[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestAlgorithms/Sorting/MediansAndStatisticsTests.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms.Sorting;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestAlgorithms.Sorting
+{
+    public class MediansAndStatisticsTests
+    {
+        private int[] _input;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _input = new[] {9, 4, 2, 7, 1, 10, 5};
+        }
+        
+        [Test]
+        public void Minimum_SomeContainer_MinimumValueFromContainer()
+        {
+            _input.Minimum().Should().Be(1);
+        }
+        
+        [Test]
+        public void Maximum_SomeContainer_MaximumValueFromContainer()
+        {
+            _input.Maximum().Should().Be(10);
+        }
+    }
+}
